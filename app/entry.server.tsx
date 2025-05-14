016620812bdb9b761eed0ba4bfd6317f2d39519a
Kevin <kevindana99@gmail.com>
2025-05-14 05:05:14 -0300

    added support styled components in ssr working

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,63 +1,29 @@
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { addDocumentResponseHeaders } from "./shopify.server";
 import { ServerStyleSheet } from "styled-components"; // Importa ServerStyleSheet
 import { EntryContext } from "@remix-run/node";
 
-const ABORT_DELAY = 5000;
-
-export default async function handleRequest(
+export default function handleBrowserRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  addDocumentResponseHeaders(request, responseHeaders);
+  const sheet = new ServerStyleSheet();
 
-  const userAgent = request.headers.get("user-agent");
-  const callbackName = isbot(userAgent ?? "") ? "onAllReady" : "onShellReady";
+  let markup = renderToString(
+    sheet.collectStyles(
+      <RemixServer context={remixContext} url={request.url} />,
+    ),
+  );
+  const styles = sheet.getStyleTags();
 
-  return new Promise((resolve, reject) => {
-    const sheet = new ServerStyleSheet(); // Crea una instancia de ServerStyleSheet
+  markup = markup.replace("__STYLES__", styles);
 
-    try {
-      // Renderiza el servidor con renderToString
-      const bodyHtml = renderToString(
-        sheet.collectStyles(
-          // Asegúrate de envolver tu componente con `collectStyles`
-          <RemixServer context={remixContext} url={request.url} />,
-        ),
-      );
+  responseHeaders.set("Content-Type", "text/html");
 
-      // Obtén los estilos generados por styled-components
-      const styles = sheet.getStyleTags();
-
-      const html = `
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Shopify App</title>
-            ${styles}  <!-- Inyecta los estilos aquí -->
-          </head>
-          <body>
-            <div id="root">${bodyHtml}</div> <!-- Aquí se inyecta el contenido generado -->
-          </body>
-        </html>
-      `;
-
-      resolve(
-        new Response(html, {
-          headers: responseHeaders,
-          status: responseStatusCode,
-        }),
-      );
-    } catch (error) {
-      reject(error);
-    } finally {
-      sheet.seal(); // Cierra la instancia de ServerStyleSheet
-    }
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
